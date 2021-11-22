65143ce99470b70bbf2bb4b4beaf43ec3b5cff8d
Nethrika01 <94846285+Nethrika01@users.noreply.github.com>
2021-11-22 19:03:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var ball;
+
+function setup() {
+  createCanvas(400,400);
+  ball=createSprite(200,200,50,50);
+}
+
+function draw() 
+{
+  
+  if(keyIsDown(RIGHT_ARROW)) {
+    ball.position.x = ball.position.x + 2;
+  }
+  if(keyIsDown(DOWN_ARROW)) {
+    ball.position.y = ball.position.y + 2;
+  }
+  if(keyIsDown(LEFT_ARROW)) {
+  background("violet")
+  }
+drawSprites(); 
+}
+
+
+
+
